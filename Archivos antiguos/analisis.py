fc124cc3c7fd8ca94a7ab5348f68e09b7c16968b
Cesar Miguel Salazar Reyes <cesar.salazar.r@uni.pe>
2026-07-22 20:15:44 -0500

    Excluir carpeta de archivos antiguos

--- Archivos antiguos/analisis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-
-df = pd.read_csv('BBDD.csv')
-print(df.head(10))
-print(df.info())
